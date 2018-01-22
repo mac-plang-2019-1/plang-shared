98f92f22c0a2d7a88bd1fe97b676cb4942ffcbb7
Paul Cantrell <cantrell@pobox.com>
2018-01-22 14:45:14 -0600

    Grammar explorer initial version

---
+++ tools/grammar-explorer/src/grammar-explorer.js
@@ -0,0 +1,162 @@
+'use strict';
+
+function elem(tag, cssClass, ...children) {
+  return $('<' + tag + '>', { class: cssClass })
+    .append(children);
+}
+
+class Literal {
+  constructor(text) {
+    this.text = text;
+  }
+
+  render() {
+    return elem("div", "literal", this.text);
+  }
+
+  renderSummary() {
+    return this.render();
+  }
+}
+
+class Substitution {
+  constructor(children) {
+    this.children = children;
+  }
+
+  get resolvedChildren() {
+    return this.children.map((child) => {
+      if(typeof child == "string")
+        return findSymbol(child);
+      else
+        return child;
+    });
+  }
+
+  render() {
+    return this.resolvedChildren.map((child) => { return child.render() });
+  }
+
+  renderSummary() {
+    return elem("div", "substitution",
+      ...this.resolvedChildren.map((child) => { return child.renderSummary() }));
+  }
+}
+
+class Choice {
+  constructor(name, choices, cssClass) {
+    this.name = name;
+    this.choices = choices.map((choice) => { return new Substitution(choice) });
+    this.cssClass = cssClass;
+  }
+
+  render() {
+    let header = elem("a", this.cssClass, this.name);
+    let chooser = elem("div", "chooser",
+      elem("div", "header", header));
+
+    let choices = elem("div", "choices",
+      ...this.choices.map((choice) => {
+        return choice.renderSummary()
+          .click(() => {
+            $(chooser).replaceWith(choice.render());
+          });
+      }));
+    chooser.append(choices)
+
+    header.click(() => { choices.toggle() });
+
+    return chooser;
+  }
+
+  renderSummary() {
+    return elem("div", this.cssClass, this.name);
+  }
+}
+
+class Repetition {
+  constructor(child) {
+    this.child = child;
+  }
+
+  render() {
+    return this.child.render();
+  }
+
+  renderSummary() {
+    return this.child.renderSummary();
+  }
+}
+
+class Nothing {
+  render() {
+    return [];
+  }
+
+  renderSummary() {
+    return elem("div", "nothing", "< nothing >");
+  }
+}
+
+class TextInput {
+  constructor(caption) {
+    this.caption = caption;
+  }
+
+  render() {
+    return this.renderSummary().prepend(
+      $('<input type="text">'));
+  }
+
+  renderSummary() {
+    return elem("div", "text-input", elem("div", "caption", this.caption));
+  }
+}
+
+function textSummary(items) {
+  return items.map((item) => {
+    return item.renderSummary ? item.renderSummary().text() : item;
+  }).join(" ");
+}
+
+function literal(text) {
+  return new Literal(text);
+}
+
+function optional(...items) {
+  return new Choice(
+    textSummary(items),
+    [
+      items,
+      [new Nothing()]
+    ],
+    "optional nonterminal");
+}
+
+function multiple(...items) {
+  let repetition = new Repetition();
+  let choice = new Choice(
+    textSummary(items),
+    [
+      items.concat(repetition),
+      [new Nothing()]
+    ],
+    "multiple nonterminal");
+  repetition.child = choice;
+  return choice;
+}
+
+function textInput() {
+  return new TextInput(...arguments);
+}
+
+let grammar = { };
+
+function findSymbol(symbolName) {
+  let result = grammar[symbolName];
+  if(Array.isArray(result)) {
+    result = new Choice(symbolName, result, "nonterminal");
+    grammar[symbolName] = result;
+  }
+  return result;
+}
